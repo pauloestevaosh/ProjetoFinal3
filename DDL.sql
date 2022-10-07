9624c74cd31aed1f0f5bea3e868cca8268222aea
pedromatos92 <peu.matos92@gmail.com>
2022-10-06 23:41:21 -0300

    versão atualizada

--- DDL.sql
+++ DDL.sql
@@ -1,54 +1,57 @@
-create database if not exists Resilia;
-
-use Resilia;
-
-
-CREATE TABLE IF NOT EXISTS Facilitadores (
-    id_facilitador INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    cpf varchar (11) NOt NULL,
-    nome VARCHAR(50) NOT NULL,
-    telefone VARCHAR(11) NOT NULL,
-    endereco VARCHAR(100),
-    cidade VARCHAR(11),
-    estado VARCHAR(11),
-    email varchar(40),
-    especialidade VARCHAR (20)
-
-);
-
-
-create table Cursos
-(
-   id_curso int PRIMARY KEY not null,
-   nome_curso varchar(50) not null,
-   data_cadastro datetime not null,
-   id_facilitador int not null,
-   FOREIGN KEY (id_facilitador) REFERENCES Facilitadores (id_facilitador)
-);
-
-CREATE TABLE Turmas (
-    id_turma int PRIMARY KEY NOT NULL,
-	id_curso int NOT NULL,
-	valor_turma float not null,
-	Data_inicio date not null,
-	data_termino date,
-	data_cadastro datetime not null,
-    FOREIGN KEY (id_curso) REFERENCES Cursos (id_curso)
-
-);
-CREATE TABLE IF NOT EXISTS Alunos (
-    id_aluno INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    cpf varchar (11) NOt NULL,
-    nome VARCHAR(50) NOT NULL,
-	data_nascimento date NOT NULL,
-    telefone VARCHAR(11) NOT NULL,
-    endereco VARCHAR(100),
-    cidade VARCHAR(11),
-    estado VARCHAR(11),
+create database if not exists resilia;
+use resilia;
+create table if not exists facilitadores (
+    id_facilitador int primary key not null auto_increment,
+    cpf varchar(11) not null,
+    nome varchar(50) not null,
+    telefone varchar(11) not null,
+    endereco varchar(100),
+    cidade varchar(50),
+    estado varchar(50),
     email varchar(50),
-    sexo varchar(1) NULL, -- M para Masculino ou F para Feminino
-    data_matricula datetime NOT NULL,
-    id_turma int,
-    FOREIGN KEY (id_turma) REFERENCES Turmas (id_turma)
+    especialidade varchar(20)
+);
+create table if not exists cursos (
+    id_curso int primary key not null,
+    nome_curso varchar(50) not null,
+    duracao_curso varchar(50) not null,
+    data_cadastro date not null
     
+);
+create table if not exists turmas (
+    id_turma int primary key not null,
+    id_curso int not null,
+    valor_turma float not null,
+    Data_inicio date not null,
+    data_termino date,
+    data_cadastro date not null,
+    id_facilitador int not null,
+    foreign key (id_curso)
+	references cursos (id_curso),
+    foreign key (id_facilitador)
+    references facilitadores (id_facilitador)
+       
+);
+create table if not exists alunos (
+    id_aluno int primary key not null auto_increment,
+    cpf varchar(11) not null,
+    nome varchar(50) not null,
+    data_nascimento date not null,
+    telefone varchar(11) not null,
+    endereco varchar(100),
+    cidade varchar(50),
+    estado varchar(50),
+    email varchar(50),
+    sexo varchar(1) not null,
+    data_matricula date not null,
+    id_turma int not null,
+    foreign key (id_turma)
+        references turmas (id_turma)
+); 
+create table if not exists experiencia_soft (
+    id_avaliacao int primary key not null auto_increment,
+    id_aluno int not null,
+    comentario varchar(500),
+    foreign key (id_aluno)
+        references alunos (id_aluno)
 );
